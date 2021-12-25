2491c53ad2f03db690ba8f3a007a4f717d86fc0a
krlleee <58256858+krlleee@users.noreply.github.com>
2021-12-25 16:51:46 +0100

    Initial Commit

---
+++ WebApplication2/Controllers/ValueController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication2.Data;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValueController : Controller
+    {
+        private readonly DataContext _context;
+
+        public ValueController(DataContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+
+            var values = await _context.Values.ToListAsync();
+
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var value = await _context.Values.FirstOrDefaultAsync(x=>x.Id==id);
+
+            return Ok(value);
+        }
+    }
+}
